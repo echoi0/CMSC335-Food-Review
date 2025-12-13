ad213d0cc1c8e2785198aae52278e2fda140db18
Eli <echoi04@terpmail.umd.edu>
2025-12-13 00:37:41 -0500

    main page wip

--- main.js
+++ main.js
@@ -17,25 +17,53 @@ const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1});
 const databaseName = "FOOD";
 const collectionName = "foodReview";
 
-const uri = process.env.MONGO_CONNECTION_STRING;
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "templates"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const url = 'https://raw.githubusercontent.com/andyklimczak/TheReportOfTheWeek-API/refs/heads/master/data/reports.json';
+const data = {};
 
-try {
-   const response = await fetch(url);
-
-   if (!response.ok) {
-       throw new Error('Error fetching data from the API');
+app.get("/", async (request, response) => {
+   try {
+      const APIresponse = await fetch(url);
+      if (!APIresponse.ok) {
+         throw new Error('Network response was not ok: ' + response.statusText)
+      }
+      data = await APIresponse.json();
+        
+      response.render("index", { data });
+   } catch (error) {
+      console.error(error);
+      response.render("index", { data: null, error: "Could not fetch data" });
    }
-      const data = await response.json();
+});
 
-} catch (error) {
-        // document.querySelector("#results").innerHTML = "Error: Could not fetch data";
-        console.error(error);
+app.post("/search", async (request, response) => {
+   const database = client.db(databaseName);
+   const collection = database.collection(collectionName);
+   const selectedReviews = request.body.reviews;
+   const username = request.body.name || "Guest";
+   const user = { name: username };
+   await collection.insertOne(user);
+});
+
+console.log(`Web server started and running at http://localhost:5000`);
+
+
+console.log("Stop to shutdown the server: ");
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim();
+    if (input === "stop") {
+        console.log("Shutting down the server");
+        process.exit(0);
+    } else {
+        console.log(`Invalid command: ${input}`);
     }
+});
 
+
+
+app.listen(5000);
