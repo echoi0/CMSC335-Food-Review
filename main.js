d3316d4cb3a1d05503d4550196e3b11903eaacee
sarahzac734 <sarahzachariah13@gmail.com>
2025-12-14 18:42:55 -0500

    added mongoose

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict"; 
 
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 require("dotenv").config({
    path: path.resolve(__dirname, "credentials/.env"),
 });
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
@@ -18,16 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 async function main() {
    const uri = process.env.MONGO_CONNECTION_STRING;
-   const databaseName = "FOOD";
-   const collectionName = "foodReview";
    const url = 'https://raw.githubusercontent.com/andyklimczak/TheReportOfTheWeek-API/refs/heads/master/data/reports.json';
    let data;
 
 try {
-      const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1});
-      await client.connect();
-      const database = client.db(databaseName);
-      const collection = database.collection(collectionName);
+     
+   await mongoose.connect(uri);
+
+   const foodSchema = new mongoose.Schema({
+      product: String
+   });
+
+   const Food = mongoose.model("Food", foodSchema);
 
 app.get("/", async (request, response) => {
    try {
@@ -63,14 +64,18 @@ app.get("/search", async (request, response) => {
       }else if (itemsSelected === "Surprise"){
          const options = ["Running on Empty", "Energy Crisis", "Drink Review"];
          itemOne = options[Math.floor(Math.random() * options.length)];
+         console.log(Math.floor(Math.random() * options.length));
       }
       let results = `<table><th>Product</th><th>Manufacturer</th><th>Rating</th><th>Youtube Video Title</th>`;
       let count = 0;
+      console.log(itemOne);
       while (count < 4){
             let num = Math.floor(Math.random()* data2.length);
             if(data2[num].category.includes(itemOne)){
             count++;
-            await collection.insertOne({ product: data2[num].product });
+            await Food.create({
+               product: data2[num].product
+            });
             results += 
             `
                   <tr>
@@ -100,29 +105,23 @@ app.get("/search", async (request, response) => {
 
 
 app.get("/recent", async (request, response) => {
-   //const database = client.db(databaseName);
-   //const collection = database.collection(collectionName);
    const filter = {};
-   const cursor = collection.find(filter);
-   const result = await cursor.toArray();
+   const cursor = await Food.find(filter);
    let answer = "";
-   result.forEach(item => answer += `<p>${item.product}</p><br>`);
-   answer += `<h2>Recent Searches: ${result.length} reviews</h2>`; 
+   cursor.forEach(item => answer += `<p>${item.product}</p><br>`);
+   answer += `<h2>Recent Searches: ${cursor.length} reviews</h2>`; 
    response.render("recent", { search: answer });
 });
 
 app.get("/clear", async (request, response) => {
-   await client.connect();
-   const database = client.db(databaseName);
-   const collection = database.collection(collectionName);
-   await collection.deleteMany({});
+   await Food.deleteMany({});
    response.render("clear");
 });
 
-console.log(`Web server started and running at http://localhost:6543`);
+console.log(`Web server started and running at http://localhost:5001`);
 
 
-console.log("Stop to shutdown the server: ");
+console.log("Enter stop to shutdown the server: ");
 process.stdin.on("data", (data) => {
     const input = data.toString().trim();
     if (input === "stop") {
@@ -137,7 +136,7 @@ process.stdin.on("data", (data) => {
       console.error(e);
     } 
 
-app.listen(6543);
-   };
+   app.listen(5001);
+};
 
    main();
